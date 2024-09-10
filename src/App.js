d14dc95f99340980ee719d52668f17e155755e6a
besher al addam <beshoux@outlook.fr>
2024-09-10 13:30:46 +0200

    Starting the weather project

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { location: "nantes" };
+    this.fetchWeather = this.fetchWeather.bind(this);
+  }
+
+  fetchWeather() {
+    console.log("Loading data");
+    console.log(this);
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <h1>Classy weather</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Search for a Location"
+            value={this.state.location}
+            onChange={(e) => this.setState({ location: e.target.value })}
+          />
+        </div>
+        <button onClick={this.fetchWeather}>Get weather</button>
+      </div>
+    );
+  }
+}
+export default App;
